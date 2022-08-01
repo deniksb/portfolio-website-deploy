368a97b460cfbf1215960deb05baf4dc988e269c
denislav.berberov <denislav.berberov@trading212.com>
2022-08-01 17:22:12 +0300

    removed numbering from skills table

--- src/components/SkillsTable.js
+++ src/components/SkillsTable.js
@@ -14,7 +14,6 @@ export const SkillsTable = () => (
   <Table className='skills-table'  wrapperClasses="table-responsive" striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
           <th>Technology</th>
           <th>Skill Level</th>
           
@@ -22,64 +21,63 @@ export const SkillsTable = () => (
       </thead>
       <tbody>
         <tr>
-          <td>1</td>
           <td>Java</td>
           <td>Intermediate</td>
           
         </tr>
         <tr>
-          <td>2</td>
+          
           <td>Java Spring Boot</td>
           <td>Intermediate</td>
           
         </tr>
         <tr>
-          <td>3</td>
+          
           <td>JavaScript</td>
           <td>Intermediate</td>
         </tr>
         <tr>
-          <td>4</td>
+          
           <td>MySQL</td>
           <td>Intermediate</td>
         </tr>
         <tr>
-          <td>5</td>
+          
           <td>HTML</td>
           <td>Intermediate</td>
         </tr>
         <tr>
-          <td>6</td>
+          
           <td>CSS</td>
           <td>Intermediate</td>
         </tr>
         <tr>
-          <td>7</td>
+          
           <td>Bootstrap</td>
           <td>Intermediate</td>
         </tr>
         <tr>
-          <td>8</td>
+          
           <td>React</td>
           <td>Beginner</td>
         </tr>
         <tr>
-          <td>9</td>
+          
           <td>PHP</td>
           <td>Beginner</td>
         </tr>
         <tr>
-          <td>10</td>
+          
           <td>Python</td>
           <td>Beginner</td>
         </tr>
         <tr>
-          <td>11</td>
+          
           <td>SCRUM</td>
           <td>Beginner</td>
         </tr>
         <tr>
-          <td>12</td>
+          
           <td>Agile</td>
           <td>Beginner</td>
         </tr>
